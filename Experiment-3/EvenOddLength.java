a696689d7971afb4a75a4330d03dd857e19c8395
RanvijaySuryawanshi <ranvijaysuryawanshi@gmail.com>
2026-07-30 10:08:48 +0530

    Update EvenOddLength.java

--- Experiment-3/EvenOddLength.java
+++ Experiment-3/EvenOddLength.java
@@ -1,6 +1,6 @@
 public class EvenOddLength{
     public static void main(String[] args){
-        String s="ranvijayO";
+        String s="ranvijay";
         if (s.length()%2 == 0)
             System.out.println("Length is Even");
         else
