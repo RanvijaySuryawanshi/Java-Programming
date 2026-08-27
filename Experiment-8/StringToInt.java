11bb225bae7206aed46de3bc2e080b8d2a66e51b
RanvijaySuryawanshi <ranvijaysuryawanshi@gmail.com>
2026-08-27 14:32:56 +0530

    Update StringToInt.java

--- Experiment-8/StringToInt.java
+++ Experiment-8/StringToInt.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class StringToInt {
-    static void convert(String str) {
+    static void convert(String s) {
         try {
-            int num = Integer.parseInt(str);
-            System.out.println("Integer value = " + num);
+            int n = Integer.parseInt(s);
+            System.out.println("Integer value = " + n);
         }
         catch (NumberFormatException e) {
             System.out.println("Invalid number. Please enter a valid integer.");
@@ -12,8 +12,8 @@ public class StringToInt {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        String str = sc.nextLine();
-        convert(str);
+        String s = sc.nextLine();
+        convert(s);
         sc.close();
     }
 }
