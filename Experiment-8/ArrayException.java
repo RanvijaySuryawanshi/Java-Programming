329af2310024a1b1b3b5c695ff30204a5acba7e8
RanvijaySuryawanshi <ranvijaysuryawanshi@gmail.com>
2026-08-27 14:33:57 +0530

    Update ArrayException.java

--- Experiment-8/ArrayException.java
+++ Experiment-8/ArrayException.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class ArrayException {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = {10, 20, 30, 40, 50};
+        int[] a = {10, 20, 30, 40, 50};
         try {
             System.out.print("Enter array index: ");
-            int index = sc.nextInt();
-            System.out.println("Element = " + arr[index]);
+            int i = sc.nextInt();
+            System.out.println("Element = " + a[i]);
         }
         catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Error: Array index is out of bounds.");
